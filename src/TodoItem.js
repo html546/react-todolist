02837fa9dffdade199954d07bf72e01b5a332227
chengjiang <15712859926@163.com>
2018-10-10 10:05:32 +0800

    first commit

---
+++ src/TodoItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+class TodoItem extends Component {
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+    handleDelete() {
+        const { deleteItem, index } = this.props;
+        // this.props.delete1(this.props.index);
+        deleteItem(index);
+    }
+    render() {
+        const { content, test } = this.props;
+        return (
+            <div onClick={this.handleDelete}>{test} - {content}</div>
+        )
+    }
+}
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    index: PropTypes.number.isRequired,
+    deleteItem: PropTypes.func.isRequired
+}
+TodoItem.defaultProps = {
+    test: 'Hello World'
+}
+export default TodoItem;
