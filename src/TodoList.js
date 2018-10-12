53837e9ec4e964565108f377e76843cd34a7d603
chengjiang <15712859926@163.com>
2018-10-12 15:38:55 +0800

    使用redux完成todolist的删除功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -28,7 +28,7 @@ class TodoList extends Component {
                     style={{ marginTop: '10px', width: '300px' }}
                     bordered
                     dataSource={this.state.list}
-                    renderItem={item => (<List.Item>{item}</List.Item>)}
+                    renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
                 />
             </div>
         )
@@ -47,6 +47,13 @@ class TodoList extends Component {
         const action = {
             type: 'add_todo_item'
         };
+        store.dispatch(action);
+    }
+    handleItemDelete(index) {
+        const action = {
+            type: 'delete_todo_item',
+            index
+        }
         store.dispatch(action);
     }
 }
