f22673ade28848036474be74a6b787964dbfaf1c
chengjiang <15712859926@163.com>
2018-10-12 11:45:16 +0800

    引入了react-transition-group组件库 使用了其中的TransitionGroup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
 import React, { Component } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './App.css';
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: true
+            list: []
         }
-        this.handleToggle = this.handleToggle.bind(this);
+        this.handleAddItem = this.handleAddItem.bind(this);
     }
     render() {
         return (
             <>
-                <CSSTransition
-                    in={this.state.show}
-                    timeout={1000}
-                    classNames='fade'
-                    unmountOnExit
-                    onEntered={(el)=>{el.style.color='blue'}}
-                    appear={true}
-                >
-                    <div>Hello</div>
-                </CSSTransition>
-                <button onClick={this.handleToggle}>Toggle</button>
+                <TransitionGroup>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <CSSTransition
+                                    timeout={1000}
+                                    classNames='fade'
+                                    unmountOnExit
+                                    onEntered={(el) => { el.style.color = 'blue' }}
+                                    appear={true}
+                                    key={index}
+                                >
+                                    <div>{item}</div>
+                                </CSSTransition>
+                            )
+                        })
+                    }
+                </TransitionGroup>
+                <button onClick={this.handleAddItem}>Toggle</button>
             </>
         )
     }
-    handleToggle() {
-        this.setState({
-            show: !this.state.show
+    handleAddItem() {
+        this.setState((prevState) => {
+            return {
+                list: [...prevState.list, 'item']
+            }
         })
     }
 }
