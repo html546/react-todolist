2cde7b68d3ee75e5d9360fd9f8ee5e628f4126fd
chengjiang <15712859926@163.com>
2018-10-12 11:32:25 +0800

    增加了react-transition-group组件库 使用了其中的CSSTransition的功能

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import './App.css';
 class App extends Component {
     constructor(props) {
@@ -11,7 +12,16 @@ class App extends Component {
     render() {
         return (
             <>
-                <div className={this.state.show ? 'show' : 'hide'}>Hello</div>
+                <CSSTransition
+                    in={this.state.show}
+                    timeout={1000}
+                    classNames='fade'
+                    unmountOnExit
+                    onEntered={(el)=>{el.style.color='blue'}}
+                    appear={true}
+                >
+                    <div>Hello</div>
+                </CSSTransition>
                 <button onClick={this.handleToggle}>Toggle</button>
             </>
         )
