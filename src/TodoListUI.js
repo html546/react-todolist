143af6deca68545f8654fb4047a84720210b777c
chengjiang <15712859926@163.com>
2018-10-12 17:04:58 +0800

    无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,9 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 
-
-class TodoListUI extends Component {
+const TodoListUI = (props) => {
+    return (
+        <div style={{ marginTop: '10px', marginLeft: '10px' }}>
+            <div>
+                <Input
+                    value={props.inputValue}
+                    placeholder="todo info"
+                    style={{ width: '300px', marginRight: '10px' }}
+                    onChange={props.handleInputChange}
+                />
+                <Button type="primary" onClick={props.handleBtnClick}>提交</Button>
+            </div>
+            <List
+                style={{ marginTop: '10px', width: '300px' }}
+                bordered
+                dataSource={props.list}
+                renderItem={(item, index) => (<List.Item onClick={(index) => { props.handleItemDelete(index) }}>{item}</List.Item>)}
+            />
+        </div>
+    )
+}
+/* class TodoListUI extends Component {
     render() {
         return (
             <div style={{ marginTop: '10px', marginLeft: '10px' }}>
@@ -25,5 +45,5 @@ class TodoListUI extends Component {
             </div>
         )
     }
-}
+} */
 export default TodoListUI;
